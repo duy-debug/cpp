079f34ff19b7808f85d918ca1bd1e522fa32bf09
Tran Mai Ngoc Duy <duy.tmn@gmail.com>
2024-10-24 16:16:59 +0700

    first commit

--- Recursion/Luythuanhiphan.cpp
+++ Recursion/Luythuanhiphan.cpp
@@ -1,14 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 // su dung ky thuat chia de tri
-int Luythuanhiphan(int a, int b){
-	if(b==0) return 1;
-	int tem=Luythuanhiphan(a,b/2);
-	if(b%2==1) return tem*tem*a;
+int Luythuanhiphan(int a, int n){
+	if(n==0) return 1;
+	int tem=Luythuanhiphan(a,n/2);
+	if(n%2==1) return tem*tem*a;
 	else return tem*tem; 
 }
 int main(){
-	int a, b;
-	cin>>a>>b;
-	cout<<Luythuanhiphan(a,b)<<endl;
+	int a, n;
+	cin>>a>>n;
+	cout<<Luythuanhiphan(a,n)<<endl;
 }
